0c4e0b7b1f6bc61da31684f07bc91c5c948b8a7a
Phạm Quốc Dũng <163796580+Dung20225817@users.noreply.github.com>
2025-06-11 22:50:59 +0700

    add managercontroller

---
+++ Program/BE/src/main/java/com/example/itssprj_ver1/controller/managerController.java
@@ -0,0 +1,54 @@
+package com.example.itssprj_ver1.controller;
+
+import com.example.itssprj_ver1.service.*;
+import com.example.itssprj_ver1.repository.userRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.sql.Date;
+
+@RestController
+@RequestMapping("/manager")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+public class managerController {
+    private final reviewService reviewService;
+    private final roomEquipmentService roomEquipmentService;
+    private final exerSession exerSession;
+    private final paymentService paymentService;
+    private final memRegService memRegService;
+    private final customerService customerService;
+    private final membershipService membershipService;
+    private final roomService roomService;
+
+    @GetMapping("/getReviews")
+    public ResponseEntity<Map<String, Object>> getReview(@RequestHeader(value = "token", required = false) String token) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            // Kiểm tra token
+            if (token == null || token.isEmpty()) {
+                response.put("success", false);
+                response.put("message", "Token is missing or invalid");
+                return ResponseEntity.badRequest().body(response);
+            }
+
+            List<Map<String, Object>> reviews = reviewService.getReview();
+            if (reviewService.getReview() != null) {
+                response.put("status", "Lấy danh sách review thành công");
+                response.put("data", reviews);
+                return ResponseEntity.ok(response);
+            } else {
+                response.put("status", "Không có review nào");
+                return ResponseEntity.status(404).body(response);
+            }
+        } catch (Exception e) {
+            response.put("message", "Đã xảy ra lỗi: " + e.getMessage());
+            return ResponseEntity.status(500).body(response);
+        }
+    }
+
+}
